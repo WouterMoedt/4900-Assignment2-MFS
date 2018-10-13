9c75ccaf09f57b3bc925dbb0df5b02e40d95f377
WouterMoedt <wouter.moedt@live.nl>
2018-10-13 10:48:56 -0500

    Django-Heroku

--- mfscrm/urls.py
+++ mfscrm/urls.py
@@ -25,8 +25,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('crm.urls')),
-    path('login/', LoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(), name="logout"),
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('login/', LoginView.as_view(), name="login"),
+    #path('logout/', LogoutView.as_view(), name="logout"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
