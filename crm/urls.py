894792054d089f8a9b26e1d801acda95925e1122
WouterMoedt <wouter.moedt@live.nl>
2018-10-26 23:13:15 -0500

    Password change fix

--- crm/urls.py
+++ crm/urls.py
@@ -6,19 +6,24 @@ app_name = 'crm'
 
 urlpatterns = [
     url(r'^home/$', views.home, name='home'),
+    # Homepage
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
+    # Customers
     path('customer_list', views.customer_list, name='customer_list'),
     path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
     path('customer/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+    # Services
     path('service_list', views.service_list, name='service_list'),
     path('service/create/', views.service_new, name='service_new'),
     path('service/<int:pk>/edit/', views.service_edit, name='service_edit'),
     path('service/<int:pk>/delete/', views.service_delete, name='service_delete'),
+    # Products
     path('product_list', views.product_list, name='product_list'),
     path('product/create/', views.product_new, name='product_new'),
     path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),
     path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
     path('customer/<int:pk>/summary/', views.summary, name='summary'),
+    # User Registration
     url('register/',views.register, name='register'),
 ]
